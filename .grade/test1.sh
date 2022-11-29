283041c52c65fcff623f7c041f24f6a828163e08
Jakub Bulín <jakub.bulin@gmail.com>
2022-11-29 22:32:53 +0100

    Fix tests

--- .grade/test1.sh
+++ .grade/test1.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-.tmp/Picat/picat golfers.pi picat golfers.pi 3 2 5 2>&1
+.tmp/Picat/picat golfers.pi 3 2 5 2>&1
 exit 0
--- .grade/test2.sh
+++ .grade/test2.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-.tmp/Picat/picat golfers.pi picat golfers.pi 2 2 4 2>&1
+.tmp/Picat/picat golfers.pi 2 2 4 2>&1
 exit 0
